1edca77a211e37e2cab2e85ca46b302131d9c66d
fumi <7133795+256x@users.noreply.github.com>
2026-03-21 06:55:25 +0900

    Fixed app list not updating

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "fumi.day.literallauncher"
         minSdk = 28
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 11
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
