b43895050b9223525efdaf9bd210964646c22737
256x <admin@fumi.day>
2026-05-10 07:02:25 +0900

    Bump version to 1.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "fumi.day.literallauncher"
         minSdk = 28
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.4.2"
+        versionCode = 19
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
